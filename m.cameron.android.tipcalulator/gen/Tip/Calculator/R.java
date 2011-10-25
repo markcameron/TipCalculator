9ec0062d374f60f545fe1e95938cb2786343ade4
Mark Cameron <budfrogfryer@gmail.com>
2011-10-25 11:03:48 +0200

    Added app Icon

--- m.cameron.android.tipcalulator/gen/Tip/Calculator/R.java
+++ m.cameron.android.tipcalulator/gen/Tip/Calculator/R.java
@@ -23,8 +23,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int blueprogress=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int seekbarthumb=0x7f020002;
+        public static final int ic_app_launcher=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int seekbarthumb=0x7f020003;
     }
     public static final class id {
         public static final int EditTextPeople=0x7f080006;
